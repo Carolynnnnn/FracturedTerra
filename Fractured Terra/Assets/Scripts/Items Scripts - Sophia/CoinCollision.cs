f1ae05987241c7a1eb6f8a01daf00d03a2f9fce9
Sophia Rizzo <rizzosophia06@gmail.com>
2026-03-21 15:03:23 -0400

    Rudimentary movement + Coins
    Started on player movement and created the coin item. Also adjusted default camera size.

---
+++ Fractured Terra/Assets/Scripts/Items Scripts - Sophia/CoinCollision.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CoinCollision : MonoBehaviour
+{
+    // Object initialization
+    public CoinManager cm; // Stores coin count
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin")) // When player collides with a coin 
+        {
+            cm.cointCount++; // Increases coin count
+        }
+    }
+}
---
+++ Fractured Terra/Assets/Scripts/Player Scripts - Simran/PlayerController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[SelectionBase]
+public class PlayerController : MonoBehaviour
+{
+    // Editor data
+    // Movement Attributes 
+    [SerializeField] float _moveSpeed = 1;
+    // Dependencies
+    [SerializeField] Rigidbody2D _rb;
+    
+    // Internal data
+    private Vector2 _moveDir = Vector2.zero;
+    private InputAction _moveAction;
+    
+    // Keybinds
+    private void Awake()
+    {
+        // Create a 2DVector composite binding for WASD
+        _moveAction = new InputAction("Move", InputActionType.Value);
+        _moveAction.AddCompositeBinding("2DVector")
+            .With("Up", "<Keyboard>/w")
+            .With("Down", "<Keyboard>/s")
+            .With("Left", "<Keyboard>/a")
+            .With("Right", "<Keyboard>/d");
+        
+        // Update move direction
+        _moveAction.performed += ctx => _moveDir = ctx.ReadValue<Vector2>();
+        _moveAction.canceled += ctx => _moveDir = Vector2.zero;
+    }
+    
+    // Input actions
+    private void OnEnable()
+    {
+        _moveAction.Enable();
+    }
+    private void OnDisable()
+    {
+        _moveAction.Disable();
+    }
+    
+    // Tick
+    private void FixedUpdate() // Used for physics system
+    {
+        _rb.linearVelocity = _moveDir.normalized * _moveSpeed;
+    }
+}

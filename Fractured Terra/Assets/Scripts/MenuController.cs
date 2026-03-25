11ac3f0c37481eba6e3cbce5efa904bff7f956f2
japneetmangat <japneet5858@gmail.com>
2026-03-25 14:15:39 -0400

    FixedMenuController

--- Fractured Terra/Assets/Scripts/MenuController.cs
+++ Fractured Terra/Assets/Scripts/MenuController.cs
@@ -8,14 +8,14 @@ public class MenuController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        menuCanvas.setActive(false);
+        menuCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab)){
-            menuCanvas.setActive(!menuCanvas.activeSelf);
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
         }
     }
 }

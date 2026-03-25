7711a6bc42ef14cf7305cf072834e8c78a6568a6
AlishaP09 <apate967@uwo.ca>
2026-03-25 19:35:39 -0400

    Add player attack script for pot interaction

---
+++ Fractured Terra/Assets/Scripts/Player Scripts/AttackHitbox.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AttackHitbox : MonoBehaviour; //Detects enemies hit by the player's attack
+
+{
+    
+    [SerializeField] private int damage = 1; //Damage given to enemies
+
+    private void OnTriggerEnter2D(Collider2D collision)
+{
+    EnemyHealth enemy = collision.GetComponent<EnemyHealth>();
+
+    if (enemy != null)
+    {
+        enemy.TakeDamage(damage); //Deal damage
+        Debug.Log("Enemy hit!"); 
+        
+    }
+}
+    
+    
+    
+}

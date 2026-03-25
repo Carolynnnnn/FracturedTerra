0302855d621dc05c28ac7ff1c1db450b5e2ecfa5
AlishaP09 <apate967@uwo.ca>
2026-03-25 07:11:15 -0400

    Add PlayerProgress system for tracking XP, coins, and armor

---
+++ Fractured Terra/Assets/Scripts/Progression/PlayerProgress.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerProgress : MonoBehaviour
+{
+    public int xp = 0;
+    public int coins = 0;
+    public int armor = 0;
+
+    public int itemCollections = 0;
+    public int npcInteractions = 0;
+    public int navigationProgress = 0;
+
+    public void RegisterActivity(ActivityType activity, int xpReward, int coinReward = 0)
+    {
+        xp += xpReward;
+        coins += coinReward;
+
+        switch (activity)
+        {
+            case ActivityType.ItemCollection:
+                itemCollections++;
+                break;
+
+            case ActivityType.NPCInteraction:
+                npcInteractions++;
+                break;
+
+            case ActivityType.EnvironmentalNavigation:
+                navigationProgress++;
+                break;
+        }
+
+        UpdateArmor();
+    }
+
+    void UpdateArmor()
+    {
+        if (xp >= 50)
+        {
+            armor = 2;
+        }
+        else if (xp >= 20)
+        {
+            armor = 1;
+        }
+        else
+        {
+            armor = 0;
+        }
+    }
+}

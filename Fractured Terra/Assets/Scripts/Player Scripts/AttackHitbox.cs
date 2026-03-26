377622cd2aae19be623343b9ebd96548dd1b7537
rpenrice <rpenrice@uwo.ca>
2026-03-25 20:38:31 -0400

    revieve
    revive

--- Fractured Terra/Assets/Scripts/Player Scripts/AttackHitbox.cs
+++ Fractured Terra/Assets/Scripts/Player Scripts/AttackHitbox.cs
@@ -1,23 +1,17 @@
 using UnityEngine;
 
-public class AttackHitbox : MonoBehaviour; //Detects enemies hit by the player's attack
-
+public class AttackHitbox : MonoBehaviour
 {
-    
-    [SerializeField] private int damage = 1; //Damage given to enemies
+    [SerializeField] private int damage = 1; // Damage given to enemies
 
     private void OnTriggerEnter2D(Collider2D collision)
-{
-    EnemyHealth enemy = collision.GetComponent<EnemyHealth>();
-
-    if (enemy != null)
     {
-        enemy.TakeDamage(damage); //Deal damage
-        Debug.Log("Enemy hit!"); 
-        
+        EnemyHealth enemy = collision.GetComponent<EnemyHealth>();
+
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage); // Deal damage
+            Debug.Log("Enemy hit!");
+        }
     }
-}
-    
-    
-    
 }

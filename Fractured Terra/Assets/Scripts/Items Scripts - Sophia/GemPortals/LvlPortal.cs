97ecfe9b1caee593996e2e6e8c357d441bad4f4e
Sophia Rizzo <rizzosophia06@gmail.com>
2026-03-31 08:53:29 -0400

    Portals update
    Portals now require a specific amount of gems to use in order to avoid errors later on

--- Fractured Terra/Assets/Scripts/Items Scripts - Sophia/GemPortals/LvlPortal.cs
+++ Fractured Terra/Assets/Scripts/Items Scripts - Sophia/GemPortals/LvlPortal.cs
@@ -8,10 +8,11 @@ public class LvlPortal : MonoBehaviour, IInteractable
     public String levelSceneName; // Holds the name of the scene which the portal can bring the player too
     public void Interact()
     {
-        if (levelNumber <= GemManager.gemCount) // If replayability of levels is iffy may need to change to ==
+        if (levelNumber == GemManager.gemCount) // Replayability of levels may be complicated, player must have exact gem count
         {
             SceneManager.LoadScene(levelSceneName); // Brings to specified scene if player has enough gems
         }
+        else Debug.Log("This portal requires exactly " + levelNumber + " gems");
     }
 
     public bool CanInteract()

f5cc52cde5992ec85ddc0b4c8740581c648ae7be
Carolynnnnn <86690361+Carolynnnnn@users.noreply.github.com>
2026-03-31 03:37:16 -0400

    Fixed House layer issues

--- Fractured Terra/Assets/Scripts/OrderInLayer.cs
+++ Fractured Terra/Assets/Scripts/OrderInLayer.cs
@@ -1,16 +1,60 @@
 using UnityEngine;
 public class OrderInLayer : MonoBehaviour
 {
-    private SpriteRenderer sr;
+    public SpriteRenderer[] objectRenderers;
+    private SpriteRenderer[] playerRenderers;
+    private int[] initialOrders;
+    public float feetOffset = -0.5f;
+    public float triggerDistance = 5f;
+
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
-        if (sr == null)
-            sr = GetComponentInChildren<SpriteRenderer>();
+        playerRenderers = GetComponentsInChildren<SpriteRenderer>();
+        initialOrders = new int[playerRenderers.Length];
+        for (int i = 0; i < playerRenderers.Length; i++)
+        {
+            initialOrders[i] = playerRenderers[i].sortingOrder;
+        }
     }
+
     void Update()
     {
-        if (sr == null) return;
-        sr.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
+        float playerFeetY = transform.position.y + feetOffset;
+        bool nearHouse = false;
+
+        foreach (SpriteRenderer obj in objectRenderers)
+        {
+            if (obj == null) continue;
+
+            float distance = Vector2.Distance(transform.position, obj.transform.position);
+            if (distance > triggerDistance) continue;
+
+            nearHouse = true;
+            float houseBottomY = obj.transform.position.y - obj.bounds.size.y / 2f;
+
+            if (playerFeetY < houseBottomY)
+                SetPlayerOrder(obj.sortingOrder + 1);
+            else
+                SetPlayerOrder(obj.sortingOrder - 1);
+        }
+
+        if (!nearHouse)
+            RestoreInitialOrders();
+    }
+
+    void SetPlayerOrder(int order)
+    {
+        for (int i = 0; i < playerRenderers.Length; i++)
+        {
+            playerRenderers[i].sortingOrder = order + (initialOrders[i] - initialOrders[0]);
+        }
+    }
+
+    void RestoreInitialOrders()
+    {
+        for (int i = 0; i < playerRenderers.Length; i++)
+        {
+            playerRenderers[i].sortingOrder = initialOrders[i];
+        }
     }
 }
